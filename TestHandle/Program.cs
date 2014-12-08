2d1590df47a3e935779fbb6e1158ac0189ab9182
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-12-08 16:46:04 +0200

    Initial import.

---
+++ TestHandle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TestHandle
+{
+	class SafeHandleTest : SafeHandle
+	{
+		public SafeHandleTest () : base(IntPtr.Zero, true)
+		{
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			bool leaked = !IsInvalid && !disposing;
+			if (leaked)
+				throw new NotImplementedException ("We dun goofed");
+
+			base.Dispose (disposing);
+		}
+
+		public override bool IsInvalid {
+			get {
+				return handle == IntPtr.Zero;
+			}
+		}
+
+		protected override bool ReleaseHandle ()
+		{
+			return true;
+		}
+	}
+
+	class MainClass
+	{
+		[DllImport("mynativefunction.dll")]
+		static extern void alloc_test(out SafeHandleTest sht);
+
+		static void TryCreateSafeHandle()
+		{
+			SafeHandleTest sht;
+			alloc_test (out sht);
+
+			// Comment this out if you want to get rid of on purpose leak.
+			// sht.Dispose ();
+		}
+
+		public static void Main (string[] args)
+		{
+			TryCreateSafeHandle ();
+			GC.Collect ();
+			GC.WaitForPendingFinalizers ();
+		}
+	}
+}
---
+++ native_app/mynativefunction.c
@@ -0,0 +1,20 @@
+﻿#include <stdlib.h>
+
+typedef struct test_struct {
+	int val;
+} test_struct;
+
+#if __GNUC__ >= 4
+# define EXTERN(type) extern \
+			 __attribute__((visibility("default"))) \
+			 type
+#elif defined(_MSC_VER)
+# define EXTERN(type) __declspec(dllexport) type
+#else
+# define EXTERN(type) extern type
+#endif
+
+EXTERN(void) alloc_test(test_struct **test)
+{
+	*test = malloc(sizeof(test_struct));
+}
